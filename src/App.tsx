b9f843fc9a64989d95155017a68d3ef1c7157d5b
Vadim Fattakhov <vadfthv@gmail.com>
2024-11-16 12:55:43 -0600

    wip: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import NavBar from './components/NavBar';
 import TimeToDepth from './pages/TimeToDepth';
 
 import TracerReport from './pages/TracerReport';
-//  ideas
-//  TimeToDepthProcessor line 75 - add min number of samples per station instead of just 50
-//  TimeToDepthProcessor line 78 in if statement
-//  calculate successful and ignored (due number of samples) stations
-//  report these numbers after conversion
+
 //  after file is read, get all sensors and let user choose offsets,
 //  then convert with the curve depth shifted
 
